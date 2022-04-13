e19a06555bb77a8bb4ec88c7e6f9d57e6e374940
ajihsan <ajihsan96@gmail.com>
2022-04-13 20:59:15 +0700

    create star schema

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import pandas as pd
 from script.mysql import MySQL
 from script.postgresql import PostgreSQL
 
+# IMPORT SQL
+from sql.query import create_table_dim, create_table_fact
+
 
 with open ('credential.json', "r") as cred:
         credential = json.load(cred)
@@ -23,5 +26,22 @@ def insert_raw_data():
   df.to_sql(name='aji_raw_covid', con=engine, if_exists="replace", index=False)
   engine.dispose()
 
+def create_star_schema():
+  postgre_auth = PostgreSQL(credential['postgresql_warehouse'])
+  conn, cursor = postgre_auth.connect(conn_type='cursor')
+
+  query_dim = create_table_dim(schema='aji')
+  cursor.execute(query_dim)
+  conn.commit()
+
+  query_fact = create_table_fact(schema='aji')
+  cursor.execute(query_fact)
+  conn.commit()
+
+  cursor.close()
+  conn.close()
+
+
 if __name__ == '__main__':
-  insert_raw_data()
+  # insert_raw_data()
+  create_star_schema()
